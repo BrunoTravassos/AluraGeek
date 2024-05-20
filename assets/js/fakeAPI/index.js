306dd4b46be44d674da5d4a5ab55eee24ec5d0dc
BrunoTravassos <brunodefrontin@gmail.com>
2024-05-20 11:55:12 -0300

    primeiro commit

---
+++ assets/js/fakeAPI/index.js
@@ -0,0 +1,223 @@
+
+export const lista = [{
+  titulo: "Cavaleiros do Zodíaco",
+  produtos: [{
+    infos: {
+      id: 1,
+      nome: "Caderno Cavaleiros de Ouro",
+      img: "https://nerdstore.com.br/wp-content/uploads/2021/11/2ef76e872b194d2cbaceee397ff838db.jpg",
+      preco: 24.90,
+      descricao: "Caderno com os cavaleiros de ouro dos Cavaleiros do Zodíaco.",
+      alt: "Caderno com a imagem dos cavaleiros de ouro"
+    }
+  },
+  {
+    infos: {
+      id: 2,
+      nome: "Copo Cavaleiros do Zodíaco",
+      img: "https://www.jbox.com.br/wp/wp-content/uploads/2020/03/cdz-zonacriativa-destacada.jpg",
+      preco: 30.99,
+      descricao: "Copo dos Cavaleiros do Zodíaco ou Cavaleiros de Ouro 500ml.",
+      alt: "Copo dos Cavaleiros do Zodíaco"
+    }
+  },
+  {
+    infos: {
+      id: 3,
+      nome: "Pack Com 12 Estátua Cavaleiros De Ouro: Os Cavaleiros Do Zodíaco Saint Seiya 10 Cm",
+      img: "https://images.tcdn.com.br/img/img_prod/460977/pack_com_12_estatua_cavaleiros_de_ouro_os_cavaleiros_do_zodiaco_saint_seiya_10_cm_mkp_121717_1_9754888d3897524b7d3fb351a2265b17.jpg",
+      preco: 367.92,
+      descricao: "Pack Com 12 Estátua Cavaleiros de Ouro: Os Cavaleiros do Zodíaco Saint Seiya 10 cm",
+      alt: "Mini estátuas dos cavaleiros de ouro"
+    }
+  },
+  {
+    infos: {
+      id: 4,
+      nome: "KIT FESTA OS CAVALEIROS DO ZODÍACO",
+      img: "https://img.elo7.com.br/product/zoom/3847D53/os-cavaleiros-do-zodiaco-kit-quadro-centro-de-mesa-os-cavaleiros-do-zodiaco.jpg",
+      preco: 60.10,
+      descricao: "KIT FESTA OS CAVALEIROS DO ZODÍACO CONTENDO: 3 QUADROS 30cm - 5 CENTRO DE MESA 20cm",
+      alt: "KIT FESTA OS CAVALEIROS DO ZODÍACO"
+    }
+  },
+  {
+    infos: {
+      id: 5,
+      nome: "Kit 18 Bonecos Cavaleiros do Zodíaco Saint Seiya Ouro Bronze Bloco de Montar",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTibDJ2vi2M1z3DlCESKUlPc4-iHEo8PR3Uw_IQylFlFwJ4ysioPxaLwh7vS8899dJVrO4&usqp=CAU",
+      preco: 120.05,
+      descricao: "Kit 18 Bonecos Cavaleiros do Zodíaco Saint Seiya Ouro Bronze Bloco de Montar",
+      alt: "Bonecos em bloco para montar"
+    }
+  },
+  {
+    infos: {
+      id: 6,
+      nome: "KIT DE CANECAS CAVALEIROS DO ZODÍACO (5 CANECAS)",
+      img: "https://cdn.awsli.com.br/2500x2500/737/737315/produto/29637015/72862630f6.jpg",
+      preco: 154.99,
+      descricao: "KIT DE CANECAS CAVALEIROS DO ZODÍACO (5 CANECAS)",
+      alt: "Canecas dos Cavaleiros."
+    }
+  },
+  {
+    infos: {
+      id: 7,
+      nome: "Funko Pop! Cavaleiros do Zodiaco Sagittarius Seiya",
+      img: "https://cdn.awsli.com.br/300x300/84/84034/produto/115010302/funko-pop--animation-cavaleiros-do-zodiaco-saint-seiya-sagittarius-seiya-811-exc-ltrxihqet7.jpg",
+      preco: 60.07,
+      descricao: "Funko Pop! Cavaleiros do Zodiaco Sagittarius Seiya",
+      alt: "Seiya usando armadura de ouro"
+    }
+  }
+  ]
+},
+{
+  titulo: "Jogos",
+  produtos: [{
+    infos: {
+      id: 1,
+      nome: "Gran Turismo 7",
+      img: "https://images.tcdn.com.br/img/img_prod/1087887/jogo_gran_turismo_7_ps5_midia_fisica_1311_1_3747c0b7193425912f5c7e393ab071aa.jpg",
+      preco: 224.55,
+      descricao: "Jogo Gran Turismo 7 - PS5 Mídia Física",
+      alt: "Jogo Gran Turismo 7 - PS5 Mídia Física"
+    }
+  },
+  {
+    infos: {
+      id: 2,
+      nome: "Ea Sports Fc 24",
+      img: "https://http2.mlstatic.com/D_NQ_NP_619806-MLA73241327181_122023-O.webp",
+      preco: 250.98,
+      descricao: "Jogo Ea Sports Fc 24 - PS5 Midia Fisica",
+      alt: "Jogo Ea Sports Fc 24 - PS5 Midia Fisica"
+    }
+  },
+  {
+    infos: {
+      id: 3,
+      nome: "Need For Speed Unbound",
+      img: "https://http2.mlstatic.com/D_NQ_NP_847370-MLB52854783660_122022-O.webp",
+      preco: 200.99,
+      descricao: "Jogo Need For Speed Unbound -  Ps5  Mídia Física",
+      alt: "Jogo Need For Speed Unbound -  Ps5  Mídia Física"
+    }
+  },
+  {
+    infos: {
+      id: 4,
+      nome: "Rims Racing",
+      img: "https://http2.mlstatic.com/D_NQ_NP_934902-MLB71281640792_082023-O.webp",
+      preco: 160.11,
+      descricao: "Jogo Rims Racing -  Ps5 Midia Fisica",
+      alt: "Jogo Rims Racing -  Ps5 Midia Fisica"
+    }
+  },
+  {
+    infos: {
+      id: 5,
+      nome: "Topspin 2k25",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSax8pGYVJNH0QZqOxhtV3NYCEPS8jK9_NkPBPin4FOCkem91iCjXPugtqfyYN1Eb379vs&usqp=CAU",
+      preco: 360.12,
+      descricao: "Jogo Topspin 2k25 - Ps5 Midia Fisica",
+      alt: "Jogo Topspin 2k25 - Ps5 Midia Fisica"
+    }
+  },
+  {
+    infos: {
+      id: 6,
+      nome: "Grid Legends",
+      img: "https://images-americanas.b2w.io/produtos/7240353112/imagens/jogo-ps5-corrida-grid-legends-midia-fisica-novo-lacrado-ea/7240353112_1_large.jpg",
+      preco: 60.13,
+      descricao: "Jogo Grid Legends -  PS5 Midia Física",
+      alt: "Jogo Grid Legends -  PS5 Midia Física"
+    }
+  },
+  {
+    infos: {
+      id: 7,
+      nome: "F1 24",
+      img: "https://images.tcdn.com.br/img/img_prod/1087887/ea_sports_f1_2024_playstation_5_pre_venda_4610_1_7ec176925ee00ab50b53c28781c367c8.jpg",
+      preco: 300.14,
+      descricao: "Jogo Ea F1 24 - PS5 Midia Fisica",
+      alt: "Jogo Ea F1 24 - PS5 Midia Fisica"
+    }
+  }
+  ]
+},
+{
+  titulo: "Box Séries",
+  produtos: [{
+    infos: {
+      id: 1,
+      nome: "Game of Thrones",
+      img: "https://a-static.mlcdn.com.br/450x450/blu-ray-box-game-of-thrones-a-serie-completa-warner/olistsp/ospi62d0avsp5eh2/7fe572c864770ac5446c3a4a52d35fd5.jpeg",
+      preco: 455.61,
+      descricao: "Blu-Ray Box - Game of Thrones - a Série Completa",
+      alt: "Blu-Ray Box - Game of Thrones - a Série Completa"
+    }
+  },
+  {
+    infos: {
+      id: 2,
+      nome: "Friends",
+      img: "https://m.media-amazon.com/images/I/816Nhq8ozrL._AC_UF1000,1000_QL80_.jpg",
+      preco: 690.11,
+      descricao: "Blu-Ray Box - Friends - a Série Completa",
+      alt: "Blu-Ray Box - Friends - a Série Completa"
+    }
+  },
+  {
+    infos: {
+      id: 3,
+      nome: "Breaking Bad",
+      img: "https://images-americanas.b2w.io/produtos/3446017966/imagens/box-blu-ray-breaking-bad-a-serie-completa-original-16-discos/3446017966_1_large.jpg",
+      preco: 600.17,
+      descricao: "Blu-Ray Box -  Breaking Bad - a Série Completa",
+      alt: "Blu-Ray Box -  Breaking Bad - a Série Completa"
+    }
+  },
+  {
+    infos: {
+      id: 4,
+      nome: "Os Cavaleiros do Zodíaco",
+      img: "https://a-static.mlcdn.com.br/450x450/blu-ray-box-os-cavaleiros-do-zodiaco-alma-de-ouro-serie-completa-playarte/perolafilmes/789892908/c78963580449d57e349ec0f884265a05.jpeg",
+      preco: 260.18,
+      descricao: "Blu-Ray Box -   Os Cavaleiros do Zodíaco - a Série Completa",
+      alt: "Blu-Ray Box -   Os Cavaleiros do Zodíaco - a Série Completa"
+    }
+  },
+  {
+    infos: {
+      id: 5,
+      nome: " Supernatural",
+      img: "https://m.media-amazon.com/images/I/71RuC9jXE3L._AC_UF350,350_QL80_.jpg",
+      preco: 780.19,
+      descricao: "Blu-Ray Box -    Supernatural - a Série Completa",
+      alt: "Blu-Ray Box -    Supernatural - a Série Completa"
+    }
+  },
+  {
+    infos: {
+      id: 6,
+      nome: "Dexter/Dexter: New Blood",
+      img: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/a818feb6-7b4f-4fb8-ac2f-93772781713a.jpg",
+      preco: 540.25,
+      descricao: "Blu-Ray Box -    Dexter/Dexter: New Blood - a Série Completa",
+      alt: "Blu-Ray Box -    Dexter/Dexter: New Blood - a Série Completa"
+    }
+  },
+  {
+    infos: {
+      id: 7,
+      nome: "Vikings",
+      img: "https://i5.walmartimages.com/seo/Vikings-The-Complete-Series-Seasons-1-6-Blu-Ray-Box-Set_a81e913d-c5d2-4129-b4ce-18964dca9940.72b8cac8b67f2b824e0bef56d7fe157d.jpeg",
+      preco: 60.21,
+      descricao: "Blu-Ray Box -    Vikings - a Série Completa",
+      alt: "Blu-Ray Box -    Vikings - a Série Completa"
+    }
+  }
+  ]
+}]
